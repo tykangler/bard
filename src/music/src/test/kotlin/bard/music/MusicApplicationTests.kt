c215b079f96afe1c2693e2637f703dfa95d60a65
Tyler Kang <tykang8402@outlook.com>
2021-01-20 11:56:28 -0800

    project scaffolding

---
+++ src/music/src/test/kotlin/bard/music/MusicApplicationTests.kt
@@ -0,0 +1,13 @@
+package bard.music
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MusicApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
