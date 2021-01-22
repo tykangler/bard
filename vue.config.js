662845923d0bed0c01d879c636e7107d9ab16c0f
Tyler Kang <tykang8402@outlook.com>
2021-01-22 11:39:56 -0800

    init vue project

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+   devServer: {
+      https: true,
+      port: 8080,
+      hot: true,
+      open: true
+   },
+   css: {
+      sourceMap: true
+   },
+   pwa: {
+      
+   }
+}
